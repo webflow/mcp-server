cb777ea1d408b6438d9b193658fba917cf1c93f2
Deep Singhvi <deep@buildwithfern.com>
2025-03-28 10:42:19 -0400

    feat: init mcp server

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# mcp-server
-Model Context Protocol (MCP) server for the Webflow Data API.
+# webflow-mcp-server
+
+Node.js server implementing Model Context Protocol (MCP) for Webflow using the [Webflow JavaScript SDK](https://github.com/webflow/js-webflow-api).
+
+## Prerequisites
+
+- Node.js
+- npm
+- A Webflow account
+- A Webflow API token
+
+## Installation Guide
+
+### 1. Obtain a Webflow API token
+
+- Log in to your Webflow account
+- Navigate to your site's settings > **Apps & Integrations**
+- Scroll to the **API access** section and click **Generate API token**
+- Pick a token name and set the token permissions
+  - For full access, select **read and write permissions for CMS, Pages, and Sites**
+- Click **Generate token**
+- Copy the generated token
+
+_Additional documentation can be found here: https://help.webflow.com/hc/en-us/articles/33961356296723-Intro-to-Webflow-s-APIs_
+
+### 2. Build the server
+
+```shell
+cd /PATH/TO/PROJECT
+npm install
+npm run build
+```
+
+### 3. Set up your MCP client
+
+Add the following to the configuration file for your MCP client e.g. **Cursor, Windsurf, or Claude Desktop**:
+
+```
+{
+  "mcpServers": {
+    "webflow": {
+      "command": "node",
+      "args": [
+        "/PATH/TO/PROJECT/dist/index.js"
+      ],
+      "env": {
+        "WEBFLOW_TOKEN": "..."
+      }
+    }
+  }
+}
+```
+
+To find the configuration file in **Cursor**:
+
+1. Open **Cursor Settings** → **MCP**
+2. Click `+ Add New MCP Server`
+
+To find the configuration file in **Windsurf**:
+
+1. Open **Windsurf Settings** → **General** → **MCP Servers**
+2. Click `+ Add MCP Server`
+
+## Development
+
+If you want to run the server in development mode, you can install dependencies and run the server using the following command:
+
+```shell
+cd /PATH/TO/PROJECT
+npm install
+npm run dev
+```
+
+Make sure to add your Webflow API token to the `.env` file:
+
+```
+# /PATH/TO/PROJECT/.env
+WEBFLOW_TOKEN=...
+```
+
+## API
+
+The following tools are made available to MCP clients:
+
+### Sites
+
+```
+sites-list: client.sites.list(...)
+sites-get: client.sites.get(...)
+sites-publish: client.sites.publish(...)
+```
+
+### Pages
+
+```
+pages-list: client.pages.list(...)
+pages-get-metadata: client.pages.getMetadata(...)
+pages-update-page-settings: client.pages.updatePageSettings(...)
+pages-get-content: client.pages.getContent(...)
+pages-update-static-content: client.pages.updateStaticContent(...)
+```
+
+### CMS
+
+```
+collections-list: client.collections.list(...)
+collections-get: client.collections.get(...)
+collections-items-create-item-live: client.collections.items.createItemLive(...)
+collections-items-update-items-live: client.collections.items.updateItemsLive(...)
+```
+
+In this MCP server implementation we have chosen to implement only [tools](https://modelcontextprotocol.io/docs/concepts/tools), not [prompts](https://modelcontextprotocol.io/docs/concepts/prompts) or [resources](https://modelcontextprotocol.io/docs/concepts/resources). Per the MCP docs:
+
+> Tools are designed to be **model-controlled**, meaning that tools are exposed from servers to clients with the intention of the AI model being able to automatically invoke them (with a human in the loop to grant approval).
+
+We currently believe giving the model maximum access to functionality is the right approach. This view is subject to change.
---
+++ src/index.ts
@@ -0,0 +1,354 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { WebflowClient } from "webflow-api";
+import { z } from "zod";
+
+// Verify WEBFLOW_TOKEN
+if (!process.env.WEBFLOW_TOKEN) {
+  throw new Error("WEBFLOW_TOKEN is missing");
+}
+
+// Create a Webflow client
+const client = new WebflowClient({
+  accessToken: process.env.WEBFLOW_TOKEN,
+});
+
+// Create an MCP server
+const server = new McpServer({
+  name: "webflow-mcp-server",
+  version: "1.0.0",
+});
+
+//
+// SITES
+//
+
+// GET https://api.webflow.com/v2/sites
+server.tool("sites-list", async () => {
+  const response = await client.sites.list();
+  return {
+    content: [{ type: "text", text: JSON.stringify(response) }],
+  };
+});
+
+// GET https://api.webflow.com/v2/sites/:site_id
+server.tool(
+  "sites-get",
+  {
+    site_id: z.string(),
+  },
+  async ({ site_id }) => {
+    const response = await client.sites.get(site_id);
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// POST https://api.webflow.com/v2/sites/:site_id/publish
+server.tool(
+  "sites-publish",
+  {
+    site_id: z.string(),
+    customDomains: z.string().array().optional(),
+    publishToWebflowSubdomain: z.boolean().optional().default(false),
+  },
+  async ({ site_id, customDomains, publishToWebflowSubdomain }) => {
+    const response = await client.sites.publish(site_id, {
+      customDomains,
+      publishToWebflowSubdomain,
+    });
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+//
+// PAGES
+//
+
+// GET https://api.webflow.com/v2/sites/:site_id/pages
+server.tool(
+  "pages-list",
+  {
+    site_id: z.string(),
+    localeId: z.string().optional(),
+    limit: z.number().optional(),
+    offset: z.number().optional(),
+  },
+  async ({ site_id, localeId, limit, offset }) => {
+    const response = await client.pages.list(site_id, {
+      localeId,
+      limit,
+      offset,
+    });
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// GET https://api.webflow.com/v2/pages/:page_id
+server.tool(
+  "pages-get-metadata",
+  {
+    page_id: z.string(),
+    localeId: z.string().optional(),
+  },
+  async ({ page_id, localeId }) => {
+    const response = await client.pages.getMetadata(page_id, { localeId });
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// body: Webflow.Page
+const WebflowPageSchema = z.object({
+  id: z.string(),
+  siteId: z.string().optional(),
+  title: z.string().optional(),
+  slug: z.string().optional(),
+  parentId: z.string().optional(),
+  collectionId: z.string().optional(),
+  createdOn: z.date().optional(),
+  lastUpdated: z.date().optional(),
+  archived: z.boolean().optional(),
+  draft: z.boolean().optional(),
+  canBranch: z.boolean().optional(),
+  isBranch: z.boolean().optional(),
+  isMembersOnly: z.boolean().optional(),
+  seo: z
+    .object({
+      title: z.string().optional(),
+      description: z.string().optional(),
+    })
+    .optional(),
+  openGraph: z
+    .object({
+      title: z.string().optional(),
+      titleCopied: z.boolean().optional(),
+      description: z.string().optional(),
+      descriptionCopied: z.boolean().optional(),
+    })
+    .optional(),
+  localeId: z.string().optional(),
+  publishedPath: z.string().optional(),
+});
+
+// PUT https://api.webflow.com/v2/pages/:page_id
+server.tool(
+  "pages-update-page-settings",
+  {
+    page_id: z.string(),
+    localeId: z.string().optional(),
+    body: WebflowPageSchema,
+  },
+  async ({ page_id, localeId, body }) => {
+    const response = await client.pages.updatePageSettings(page_id, {
+      localeId,
+      body,
+    });
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// GET https://api.webflow.com/v2/pages/:page_id/dom
+server.tool(
+  "pages-get-content",
+  {
+    page_id: z.string(),
+    localeId: z.string().optional(),
+    limit: z.number().optional(),
+    offset: z.number().optional(),
+  },
+  async ({ page_id, localeId, limit, offset }) => {
+    const response = await client.pages.getContent(page_id, {
+      localeId,
+      limit,
+      offset,
+    });
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// nodes: Webflow.PageDomWriteNodesItem[]
+const WebflowPageDomWriteNodesItemSchema = z
+  .union([
+    z.object({
+      nodeId: z.string(),
+      text: z.string(),
+    }),
+    z.object({
+      nodeId: z.string(),
+      propertyOverrides: z.array(
+        z.object({
+          propertyId: z.string(),
+          text: z.string(),
+        })
+      ),
+    }),
+  ])
+  .array();
+
+// POST https://api.webflow.com/v2/pages/:page_id/dom
+server.tool(
+  "pages-update-static-content",
+  {
+    page_id: z.string(),
+    localeId: z.string(),
+    nodes: WebflowPageDomWriteNodesItemSchema,
+  },
+  async ({ page_id, localeId, nodes }) => {
+    const response = await client.pages.updateStaticContent(page_id, {
+      localeId,
+      nodes,
+    });
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+//
+// CMS
+//
+
+// GET https://api.webflow.com/v2/sites/:site_id/collections
+server.tool(
+  "collections-list",
+  {
+    site_id: z.string(),
+  },
+  async ({ site_id }) => {
+    const response = await client.collections.list(site_id);
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// GET https://api.webflow.com/v2/collections/:collection_id
+server.tool(
+  "collections-get",
+  {
+    collection_id: z.string(),
+  },
+  async ({ collection_id }) => {
+    const response = await client.collections.get(collection_id);
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// request: Webflow.collections.ItemsCreateItemLiveRequest
+const WebflowCollectionsItemsCreateItemLiveRequestSchema = z.union([
+  z.object({
+    id: z.string().optional(),
+    cmsLocaleId: z.string().optional(),
+    lastPublished: z.string().optional(),
+    lastUpdated: z.string().optional(),
+    createdOn: z.string().optional(),
+    isArchived: z.boolean().optional(),
+    isDraft: z.boolean().optional(),
+    fieldData: z.record(z.any()).and(
+      z.object({
+        name: z.string(),
+        slug: z.string(),
+      })
+    ),
+  }),
+  z.object({
+    items: z
+      .array(
+        z.object({
+          id: z.string().optional(),
+          cmsLocaleId: z.string().optional(),
+          lastPublished: z.string().optional(),
+          lastUpdated: z.string().optional(),
+          createdOn: z.string().optional(),
+          isArchived: z.boolean().optional(),
+          isDraft: z.boolean().optional(),
+          fieldData: z.record(z.any()).and(
+            z.object({
+              name: z.string(),
+              slug: z.string(),
+            })
+          ),
+        })
+      )
+      .optional(),
+  }),
+]);
+
+// POST https://api.webflow.com/v2/collections/:collection_id/items/live
+server.tool(
+  "collections-items-create-item-live",
+  {
+    collection_id: z.string(),
+    request: WebflowCollectionsItemsCreateItemLiveRequestSchema,
+  },
+  async ({ collection_id, request }) => {
+    const response = await client.collections.items.createItemLive(
+      collection_id,
+      request
+    );
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// request: Webflow.collections.ItemsUpdateItemsLiveRequest
+const WebflowCollectionsItemsUpdateItemsLiveRequestSchema = z.object({
+  items: z
+    .array(
+      z.object({
+        id: z.string(),
+        cmsLocaleId: z.string().optional(),
+        lastPublished: z.string().optional(),
+        lastUpdated: z.string().optional(),
+        createdOn: z.string().optional(),
+        isArchived: z.boolean().optional(),
+        isDraft: z.boolean().optional(),
+        fieldData: z
+          .record(z.any())
+          .and(
+            z.object({
+              name: z.string().optional(),
+              slug: z.string().optional(),
+            })
+          )
+          .optional(),
+      })
+    )
+    .optional(),
+});
+
+// PATCH https://api.webflow.com/v2/collections/:collection_id/items/live
+server.tool(
+  "collections-items-update-items-live",
+  {
+    collection_id: z.string(),
+    request: WebflowCollectionsItemsUpdateItemsLiveRequestSchema,
+  },
+  async ({ collection_id, request }) => {
+    const response = await client.collections.items.updateItemsLive(
+      collection_id,
+      request
+    );
+    return {
+      content: [{ type: "text", text: JSON.stringify(response) }],
+    };
+  }
+);
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
